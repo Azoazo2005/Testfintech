7704b453dfdaa12818ea95353a0707e5e3fcb351
hanifa05-hack <abdallahhanifaisidp@groupeisi.com>
2026-03-07 14:46:37 +0100

    Ajout du fichier login.php terminé

---
+++ api/auth/login.php
@@ -0,0 +1,18 @@
+<?php 
+session_start(); 
+header('Content-Type: application/json'); 
+require_once '../../core/Auth.php'; 
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') { 
+    echo json_encode(['success' => false, 'message' => 'Méthode non autorisée']); 
+    exit; 
+} 
+
+$username = $_POST['username'] ?? ''; 
+$password = $_POST['password'] ?? ''; 
+
+$auth = new Auth(); 
+$result = $auth->login($username, $password); 
+
+echo json_encode($result); 
+?>
